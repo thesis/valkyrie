77825d5c4ab042c74ee2a99396de7dbac4c6a785
Antonio Salazar Cardozo <antonio@keep.network>
2018-05-10 16:31:14 -0400

    Fix create.sh to reference stateful set
    The reference was still to redis-deployment.yaml, which no longer exists.

--- k8s/create.sh
+++ k8s/create.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-kubectl apply --record -f "${BASH_SOURCE%/*}/redis-deployment.yaml"
+kubectl apply --record -f "${BASH_SOURCE%/*}/redis-stateful-set.yaml"
 kubectl apply --record -f "${BASH_SOURCE%/*}/redis-service.yaml"
 kubectl apply --record -f "${BASH_SOURCE%/*}/hubot-deployment.yaml"
 kubectl apply --record -f "${BASH_SOURCE%/*}/hubot-http-service.yaml"
