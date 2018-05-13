638016c1a239f0a0e2bf8fe244b60900d44ca336
Antonio Salazar Cardozo <antonio@keep.network>
2018-05-12 20:31:39 -0400

    Fix unknown build text for current-build
    We were getting a vestigial note like "Released build !"; now we do
    "Released unknown build!". This should only really happen with local
    builds that have the room id for announcements set, which should be
    rare.

--- scripts/current-build.js
+++ scripts/current-build.js
@@ -22,7 +22,10 @@ try {
     console.error("Error reading buildNumber file: " + e)
 }
 let buildNumber = buildNumberBuffer.toString().trim()
-let buildString = `build [${buildNumber}](https://circle-ci.com/gh/cardforcoin/heimdall/${buildNumber})`
+let buildString =
+    buildNumber ?
+        `build [${buildNumber}](https://circle-ci.com/gh/cardforcoin/heimdall/${buildNumber})` :
+        `unknown build`
 
 let releaseNotificationRoom = process.env['RELEASE_NOTIFICATION_ROOM']
 
