ae65768d50893be7e0d9f8c1ce4e9b8c6ca46aa7
Antonio Salazar Cardozo <antonio@keep.network>
2018-10-26 10:27:36 -0400

    Add basic Flowdock API wrapper
    Right now all it provides is a flowdock.Session and a couple of helpers
    for posting an activity and a discussion, along with associated types.
    The session expects to work with a token, and doesn't support OAuth app
    setup or anything at the moment.

---
+++ lib/flowdock.ts
@@ -0,0 +1,100 @@
+import {default as axios,AxiosResponse,AxiosRequestConfig} from 'axios';
+
+const URLs = {
+    messages: `https://api.flowdock.com/messages`
+};
+
+type Thread = {
+    title: string
+    external_url: string
+    body: string
+    fields: [{ label: string, value: string }]
+}
+
+type MessageAuthor = {
+    name: string
+    email?: string
+    avatar?: string
+}
+
+type ActivityThread = {
+    title: string
+    author: MessageAuthor
+    external_thread_id: string
+    external_url: string
+
+    thread: Thread
+}
+
+type ActivityMessage = {
+    uuid: string
+    title: string
+    author: MessageAuthor
+    external_thread_id: string
+}
+
+type DiscussionMessage = {
+    uuid: string
+    title: string
+    body: string
+    author: MessageAuthor
+    external_thread_id: string
+}
+
+/**
+ * FlowdockSession represents a session of interactions with a Flowdock server.
+ * This session is authenticated by the `apiToken` passed to the constructor.
+ */
+class Session {
+    private postFn: (url:string,data?:any,config?:AxiosRequestConfig)=>Promise<AxiosResponse>
+
+    constructor(private apiToken: string, doPost: boolean = true) {
+        if (doPost) {
+            this.postFn = axios.post.bind(axios)
+        } else {
+            this.postFn = (url: string, data?: any, config?: AxiosRequestConfig) => {
+                return new Promise<AxiosResponse>((resolve) => {
+                    setTimeout(() =>  {
+                        console.log(
+                            `Flowdock POST to URL ${url} with data:\n`,
+                            `${JSON.stringify(data)}\nand config:\n${JSON.stringify(config)}`
+                        );
+                
+                        resolve({
+                            data: "",
+                            status: 200,
+                            statusText: "",
+                            headers: {},
+                            config: config
+                        })
+                    }, 1000)
+                })
+            }
+        }
+    }
+
+    async postActivity(message: ActivityThread | ActivityMessage) {
+
+        return this.postFn(
+            URLs.messages,
+            {
+                flow_token: this.apiToken,
+                event: 'activity',
+                ...message
+            }
+        )
+    }
+
+    async postDiscussion(message: DiscussionMessage) {
+        return this.postFn(
+            URLs.messages,
+            {
+                flow_token: this.apiToken,
+                event: 'discussion',
+                ...message
+            }
+        )
+    }
+}
+
+export { Session }
