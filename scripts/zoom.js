5d766f144ec55b5dd875f7c43bdd960ee79e6950
Kristen Borges <kb0rg@users.noreply.github.com>
2019-06-19 13:32:54 -0700

    Add functions to watch meeting details, respond
    If meeting never starts after specified delay, we stop watching
    If meeting starts and ends, we prompt the user to post notes in flow
    If meeting starts, but does not end within specified delay, we assume
    it still happened, and send a prompt anyway
    If there are errors at any point in this process, we log them, and in
    some cases send user a message about the error

--- scripts/zoom.js
+++ scripts/zoom.js
@@ -14,6 +14,101 @@ const zoom = require("../lib/zoom"),
 
 /** @type zoom.Session */
 let ZOOM_SESSION = null
+// temp numbers for testing
+const INTERVAL_DELAY = 5000 //30 * 1000 //
+const MEETING_START_TIMEOUT_DELAY = 1 * 60 * 1000 // 10 * 60 * 1000 // we'll only watch this long if meeting doesn't start
+const MEETING_DURATION_TIMEOUT_DELAY = 2 * 60 * 1000 // 60 * 60 * 1000 // approx max mtg duration
+
+function isMeetingStarted(meeting) {
+  return zoom
+    .getMeetingDetails(ZOOM_SESSION.token, meeting.id, meeting.uuid)
+    .then(meetingDetail => {
+      console.log(`\n\n\n##############\nSTARTED? meetingDetail`)
+      console.log(require("util").inspect(meetingDetail))
+      if ("status" in meetingDetail && meetingDetail.status === "started") {
+        return true
+      } else {
+        return false
+      }
+    })
+}
+
+// TODO: add hasStarted param & condition, otherwise this is a lie if called by itself
+function isMeetingFinished(meeting, meetingDidStart) {
+  return zoom
+    .getMeetingDetails(ZOOM_SESSION.token, meeting.id, meeting.uuid)
+    .then(meetingDetail => {
+      console.log(`\n\n\n##############\nENDED? meetingDetail`)
+      console.log(require("util").inspect(meetingDetail))
+      if (
+        "status" in meetingDetail &&
+        meetingDetail.status == "waiting" &&
+        meetingDidStart === true
+      ) {
+        return true
+      } else {
+        return false
+      }
+    })
+}
+
+function watchMeeting(meeting) {
+  let startIntervalIdPromise = new Promise((resolve, reject) => {
+    let startIntervalId = setInterval(function() {
+      isMeetingStarted(meeting)
+        .then(isStarted => {
+          if (isStarted === true) {
+            clearInterval(startIntervalId)
+            clearTimeout(startTimeoutId)
+            resolve(true)
+          }
+        })
+        .catch(err => {
+          reject(
+            `Something went wrong setting up START watch interval: ${util.inspect(
+              err,
+            )}`,
+          )
+          return
+        })
+    }, INTERVAL_DELAY)
+    let startTimeoutId = setTimeout(() => {
+      clearInterval(startIntervalId)
+      resolve("never started")
+    }, MEETING_START_TIMEOUT_DELAY)
+  })
+  return startIntervalIdPromise.then(meetingStartStatus => {
+    let endIntervalIdPromise = new Promise((resolve, reject) => {
+      if (meetingStartStatus === "never started") {
+        resolve("never started")
+        return
+      }
+      let endIntervalId = setInterval(function() {
+        isMeetingFinished(meeting, meetingStartStatus)
+          .then(isFinished => {
+            if (isFinished === true) {
+              clearInterval(endIntervalId)
+              clearTimeout(endTimeoutId)
+              resolve(null)
+            }
+          })
+          .catch(err => {
+            reject(
+              `Something went wrong setting up END watch interval: ${util.inspect(
+                err,
+              )}`,
+            )
+            return
+          })
+      }, INTERVAL_DELAY)
+      let endTimeoutId = setTimeout(() => {
+        clearInterval(endIntervalId)
+        resolve(null)
+      }, MEETING_DURATION_TIMEOUT_DELAY)
+    })
+    return endIntervalIdPromise
+  })
+}
 
 module.exports = function(robot) {
   zoom
@@ -34,6 +129,7 @@ module.exports = function(robot) {
         res.send(
           `All set; open in [the app](${meeting.app_url}) or [your browser](${meeting.join_url})!`,
         )
+        return meeting
       })
       .catch(err => {
         robot.logger.error(
@@ -41,6 +137,33 @@ module.exports = function(robot) {
           util.inspect(err),
         )
         res.send("Uh-oh, there was an issue finding an available meeting :(")
+        return
+      })
+      .then(meeting => {
+        robot.logger.info(`Start watching meeting: ${meeting.id}`)
+        return watchMeeting(meeting)
+          .then(fulfilledPromise => {
+            if (fulfilledPromise && fulfilledPromise === "never started") {
+              // log, but do not send flowdock prompt
+              robot.logger.info(
+                `This meeting looks like it never started: ${meeting.id}`,
+              )
+              return
+            }
+            // otherwise, send flowdock prompt
+            res.send(`@${res.message.user.name} Please post call notes!`)
+            robot.logger.info(`Stopped watching meeting: ${meeting.id}`)
+          })
+          .catch(err => {
+            robot.logger.error(
+              `Failed to fetch meeting details for ${meeting.id}. ERR:`,
+              util.inspect(err),
+            )
+            // We assume the meeting still happened, so we still want to send:
+            res.send(
+              `@${res.message.user.name} Don't forget to post meeting notes when your call ends!`,
+            )
+          })
       })
   })
 }
