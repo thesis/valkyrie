6f57f5913d0590446e180971a377b8f60c1e1111
Kristen Borges <kb0rg@users.noreply.github.com>
2019-07-19 15:13:03 -0700

    Create users command
    Uses flowdock or robot param, to get user lists from either of the
    sources
    However, the robot adapter is not getting User info directly from
    flowdock, it's getting it from the robot brain -- which defeats the
    purpose of comparing the contents of the brain against the data
    available to Flowdock
    
    Need to update this command to use the flowdock API

--- scripts/flowdock-admin.js
+++ scripts/flowdock-admin.js
@@ -9,9 +9,9 @@
 //
 // Author:
 //   shadowfiend
+//   kb0rg
 
 module.exports = function(robot) {
-
   robot.respond(/flows/, response => {
     if (robot.adapter.flows != null) {
       response.send(
@@ -21,6 +21,28 @@ module.exports = function(robot) {
       )
     } else {
       response.send("Not using flowdock.")
+    }
+  })
+
+  robot.respond(/users (robot|flowdock)/i, response => {
+    let dataSource = response.match[1].toLowerCase()
+    if (dataSource === "flowdock") {
+      // TODO: get users from Flowdock API
+      if (robot.adapterName.toLowerCase() === "flowdock") {
+        return response.send(
+          "TODO: get users from Flowdock API (turns out the adapter just gets it from the brain anyway)\n:shrug:",
+        )
+      } else {
+        return response.send("Not using flowdock.")
+      }
+    }
+
+    if (robot.brain.users() != null) {
+      return response.send(
+        Object.values(robot.brain.users())
+          .map(user => ` - ${user.name}: ${user.id}`)
+          .join("\n"),
+      )
     }
   })
 }
