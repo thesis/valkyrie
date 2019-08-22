9f015690ba99c3e0e8b47d1f84bcca714ea6d2bc
Kristen Borges <kb0rg@users.noreply.github.com>
2019-08-21 22:37:51 -0700

    Clean up after decaffeinate

--- test/example-test.js
+++ test/example-test.js
@@ -1,8 +1,3 @@
-/*
- * decaffeinate suggestions:
- * DS102: Remove unnecessary code created because of implicit returns
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
- */
 const Helper = require("hubot-test-helper")
 const chai = require("chai")
 
@@ -12,38 +7,34 @@ const helper = new Helper("../scripts/example.coffee")
 
 describe("example script", function() {
   beforeEach(function() {
-    return (this.room = helper.createRoom())
+    this.room = helper.createRoom()
   })
 
   afterEach(function() {
-    return this.room.destroy()
+    this.room.destroy()
   })
 
   it("doesn't need badgers", function() {
-    return this.room.user
-      .say("alice", "did someone call for a badger?")
-      .then(() => {
-        return expect(this.room.messages).to.eql([
-          ["alice", "did someone call for a badger?"],
-          ["hubot", "Badgers? BADGERS? WE DON'T NEED NO STINKIN BADGERS"],
-        ])
-      })
+    this.room.user.say("alice", "did someone call for a badger?").then(() => {
+      expect(this.room.messages).to.eql([
+        ["alice", "did someone call for a badger?"],
+        ["hubot", "Badgers? BADGERS? WE DON'T NEED NO STINKIN BADGERS"],
+      ])
+    })
   })
 
   it("won't open the pod bay doors", function() {
-    return this.room.user
-      .say("bob", "@hubot open the pod bay doors")
-      .then(() => {
-        return expect(this.room.messages).to.eql([
-          ["bob", "@hubot open the pod bay doors"],
-          ["hubot", "@bob I'm afraid I can't let you do that."],
-        ])
-      })
+    this.room.user.say("bob", "@hubot open the pod bay doors").then(() => {
+      expect(this.room.messages).to.eql([
+        ["bob", "@hubot open the pod bay doors"],
+        ["hubot", "@bob I'm afraid I can't let you do that."],
+      ])
+    })
   })
 
-  return it("will open the dutch doors", function() {
-    return this.room.user.say("bob", "@hubot open the dutch doors").then(() => {
-      return expect(this.room.messages).to.eql([
+  it("will open the dutch doors", function() {
+    this.room.user.say("bob", "@hubot open the dutch doors").then(() => {
+      expect(this.room.messages).to.eql([
         ["bob", "@hubot open the dutch doors"],
         ["hubot", "@bob Opening dutch doors"],
       ])
