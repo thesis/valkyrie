2e593b14c47267d8a9139bee80ce8c5fee733bd0
Kristen Borges <kb0rg@users.noreply.github.com>
2019-08-23 12:22:15 -0700

    Remove delays
    The missing bot replies earlier were not related to delay

--- test/example-test.js
+++ test/example-test.js
@@ -11,24 +11,24 @@ const expect = require("chai").expect
 
 describe("Testing example scripts", function() {
   chat
-    .when("user mentions a badger", { answerDelay: 200 })
+    .when("user mentions a badger")
     .user("alice")
-    .messagesRoom("did someone call for a badger?", 400)
+    .messagesRoom("did someone call for a badger?")
     .bot.messagesRoom("Badgers? BADGERS? WE DON'T NEED NO STINKIN BADGERS")
     .expect("doesn't need badgers")
 
   chat
-    .when("user asks about pod bay doors", { answerDelay: 200 })
+    .when("user asks about pod bay doors")
     .user("bob")
-    .messagesBot("open the pod bay doors", 400)
+    .messagesBot("open the pod bay doors")
     // Note: repliesWith will prepend "@<username> ": don't include in expected output
     .bot.repliesWith("I'm afraid I can't let you do that.")
     .expect("won't open the pod bay doors")
 
   chat
-    .when("user asks about dutch doors", { answerDelay: 200 })
+    .when("user asks about dutch doors")
     .user("hal")
-    .messagesBot("open the dutch doors", 400)
+    .messagesBot("open the dutch doors")
     // Alternately, messagesRoom WITH "@<username> " in expected output works
     .bot.messagesRoom("@hal Opening dutch doors")
     .expect("will open the dutch doors")
