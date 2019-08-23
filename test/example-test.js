530c325bde854f94a37c15e94fc90acd32ab1659
Kristen Borges <kb0rg@users.noreply.github.com>
2019-08-22 17:54:17 -0700

    Try messagesRoom
    Same failure

--- test/example-test.js
+++ test/example-test.js
@@ -13,7 +13,7 @@ describe("example script", function() {
   chat
     .when("user mentions a badger", { answerDelay: 200 })
     .user("alice")
-    .messagesBot("did someone call for a badger?", 400)
+    .messagesRoom("did someone call for a badger?", 400)
     .bot.repliesWith("Badgers? BADGERS? WE DON'T NEED NO STINKIN BADGERS")
     .expect("doesn't need badgers")
 })
