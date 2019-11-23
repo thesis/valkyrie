32413622f877d0e684745dd9959556d9d9767ecb
Sloan Thompson <sloan.thompson@thesis.co>
2019-11-23 07:19:42 -0500

    First pass, basic keep-test faucet
    We run a handful of internal ethereum chains, they're a pain in the ass
    to fund accounts on.  You have to use the geth console, or geth CLI to
    transfer eth.  Our keep-test environment has a public transaction node.
    This means we can take advantage of Heimdall to fund accounts on this
    chain, instead of having to use the aforementioned methods.  Here we
    provide a very basic Heimdall command that funds a passed account with
    10 eth.

---
+++ scripts/keep-test-eth-faucet.js
@@ -0,0 +1,70 @@
+// Description:
+//  Get some cold hard fake ETH from the keep-test private ETH chain.
+//  This is an extraordinary basic first pass, most things are hardcoded with purpose.
+//
+//
+// Commands:
+//   keep-test-faucet [fund] "ETH account address"
+//
+// Author:
+//   sthompson22
+//   with referenced hubot code examples from: kb0rg
+//
+
+const Web3 = require('web3');
+
+// ETH host info
+const ethHost = 'http://eth-tx.test.keep.network'
+const ethRpcPort = '8545';
+const ethNetworkId = '1101';
+
+// ETH account info
+const purse = '0x0f0977c4161a371b5e5ee6a8f43eb798cd1ae1db';
+// These are throw away accounts on an internal private testnet, hence the plaintext.
+const ethAccountPassword = 'doughnut_armenian_parallel_firework_backbite_employer_singlet';
+const etherToTransfer = '10';
+
+
+// We override transactionConfirmationBlocks and transactionBlockTimeout because they're
+// 25 and 50 blocks respectively at default.  The result of this on small private testnets
+// is long wait times for scripts to execute.
+
+const web3_options = {
+    defaultBlock: 'latest',
+    defaultGas: 4712388,
+    transactionBlockTimeout: 25,
+    transactionConfirmationBlocks: 3,
+    transactionPollingTimeout: 480
+};
+
+const web3 = new Web3(new Web3.providers.HttpProvider(ethHost + ':' + ethRpcPort), null, web3_options);
+
+module.exports = async function(robot) {
+  robot.respond(/keep-test-faucet fund (.*)/i, async function(msg) {
+    let account = msg.match[1];
+    let transferAmount = web3.utils.toWei(etherToTransfer, "ether");
+
+    if (!/^(0x)?[0-9a-f]{40}$/i.test(account)) {
+      // check if it has the basic requirements of an address
+      // double thanks to the Ethereum folks for figuring this regex out already
+      return msg.send('Improperly formatted account address, please try a valid one.');
+    }
+    try {
+      msg.send(`Unlocking purse account: ${purse}`);
+      await web3.eth.personal.unlockAccount(purse, ethAccountPassword, 150000);
+      msg.send(`Account ${purse} unlocked!`);
+    } catch (error) {
+        robot.logger.error(`ETH account unlock error: ${error.message}`)
+        msg.send('There was an issue unlocking the purse account, ask for an adult!')
+    }
+
+    try {
+      msg.send(`Funding account ${account} with ${etherToTransfer} ETH from purse: ${purse}.  Don't panic, this may take several seconds.`);
+      await web3.eth.sendTransaction({ from:purse, to:account, value:transferAmount });
+      msg.send(`Account ${account} funded!`);
+    } catch (error) {
+      robot.logger.error(`ETH account funding error: ${error.message}`)
+      msg.send('There was an issue funding the ETH account, ask for an adult!')
+    }
+  });
+};
