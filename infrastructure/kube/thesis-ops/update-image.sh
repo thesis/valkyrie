4078a0e552675b7b170652c23af425250b66d048
Kristen Borges <kb0rg@users.noreply.github.com>
2019-12-09 12:50:56 -0800

    Add ssh setup to use jumphost

--- infrastructure/kube/thesis-ops/update-image.sh
+++ infrastructure/kube/thesis-ops/update-image.sh
@@ -1,4 +1,33 @@
 #!/bin/sh
 set -e
 
-kubectl set image deployment/heimdall-hubot-deployment hubot=gcr.io/thesis-ops-2748/heimdall:$1
+if [[ -z $GOOGLE_PROJECT_NAME || -z $GOOGLE_PROJECT_ID || -z $BUILD_TAG || -z $GOOGLE_REGION || -z $GOOGLE_COMPUTE_ZONE_A  || -z $GCR_REGISTRY_URL ]]; then
+  echo "one or more required variables are undefined"
+  exit 1
+fi
+
+UTILITYBOX_IP=$(gcloud compute instances --project $GOOGLE_PROJECT_ID describe $GOOGLE_PROJECT_NAME-utility-box --zone $GOOGLE_COMPUTE_ZONE_A --format json | jq .networkInterfaces[0].networkIP -r)
+
+# Setup ssh environment
+gcloud compute config-ssh --project $GOOGLE_PROJECT_ID -q
+cat >> ~/.ssh/config << EOF
+Host *
+  StrictHostKeyChecking no
+Host utilitybox
+  HostName $UTILITYBOX_IP
+  IdentityFile ~/.ssh/google_compute_engine
+  ProxyCommand ssh -W %h:%p $GOOGLE_PROJECT_NAME-jumphost.$GOOGLE_COMPUTE_ZONE_A.$GOOGLE_PROJECT_ID
+EOF
+
+# Run migration
+ssh utilitybox << EOF
+  set -e
+  echo "<<<<<<START Download Kube Creds START<<<<<<"
+  echo "gcloud container clusters get-credentials $GOOGLE_PROJECT_NAME --region $GOOGLE_REGION --internal-ip --project=$GOOGLE_PROJECT_ID"
+  gcloud container clusters get-credentials $GOOGLE_PROJECT_NAME --region $GOOGLE_REGION --internal-ip --project=$GOOGLE_PROJECT_ID
+  echo ">>>>>>FINISH Download Kube Creds FINISH>>>>>>"
+  echo "<<<<<<START Run Heimdall Deployment START<<<<<<"
+  kubectl set image deployment/heimdall-hubot-deployment hubot=$GCR_REGISTRY_URL/$GOOGLE_PROJECT_ID/heimdall:$BUILD_TAG
+  echo ">>>>>>FINISH Run Heimdall Deployment FINISH>>>>>>"
+
+EOF
