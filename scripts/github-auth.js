0e59e450395c2cfe414226f795c868f933fe6d61
Antonio Salazar Cardozo <antonio.salazarcardozo@thesis.co>
2019-12-19 11:50:04 -0500

    Add withConfigOrReportIssues via TypeScript conversion
    The TypeScript conversion brings a couple of additional changes.
    fetchConfigOrReportIssue and withConfigOrReportIssue stop relying
    directly on a robot, instead relying on an `issueReporter`, which takes
    an error message and reports the issue as the issue reporter wishes.
    
    withConfigOrReportIssue introduces a way to group many
    fetchConfigOrReportIssue calls and require that they all succeeds before
    running some additional code. github-auth and zoom are both ported to
    use it instead of directly calling fetchConfigOrReportIssue. They no
    longer set up their respective Hubot responders if the required
    configuration isn't available.
    
    Last but not least, we gain an issueReporterForRobot that wraps a robot
    into a function suitable for passing as an issue reporter for
    fetchConfigOrReportIssue and withConfigOrReportIssues.

--- scripts/github-auth.js
+++ scripts/github-auth.js
@@ -15,117 +15,124 @@
 let passport = require("passport"),
   UUIDV4 = require("uuid/v4"),
   cookieParser = require("cookie-parser"),
-  GitHubStrategy = require("passport-github2").Strategy
+  GitHubStrategy = require("passport-github2").Strategy,
+  withConfigOrReportIssues = require("../lib/config").withConfigOrReportIssues
 
 let HOST = process.env["HUBOT_HOST"],
   SECOND = 1000,
   MINUTE = 60 * SECOND
 
 module.exports = function(robot) {
-  robot.router.use(cookieParser())
-  passport.use(
-    new GitHubStrategy(
-      {
-        clientID: process.env["GITHUB_CLIENT_ID"],
-        clientSecret: process.env["GITHUB_CLIENT_SECRET"],
-        callbackURL: `${HOST}/github/auth`,
-        userAgent: "https://thesis.co",
-      },
-      function(accessToken, refreshToken, profile, done) {
-        done(null, { token: accessToken, profile: profile })
-      },
-    ),
-  )
-
-  function cleanPending() {
-    let now = new Date().getTime(),
-      pendingGitHubTokens = robot.brain.get("pendingGitHubTokens") || {}
-
-    for (let [userID, pendingInfo] of Object.entries(pendingGitHubTokens)) {
-      if (now - pendingInfo.date > 5 * MINUTE) {
-        delete robot.brain.pendingGitHubTokens[userID]
-      }
-    }
-
-    robot.brain.set("pendingGitHubTokens", pendingGitHubTokens)
-  }
-
-  setInterval(cleanPending, 30 * SECOND)
-  cleanPending()
-
-  robot.respond(/github auth/, res => {
-    let user = res.message.user,
-      token = UUIDV4()
-
-    let pendingGitHubTokens = robot.brain.get("pendingGitHubTokens") || {}
-    pendingGitHubTokens[user.id] = {
-      token: token,
-      date: new Date().getTime(),
-    }
-    robot.brain.set("pendingGitHubTokens", pendingGitHubTokens)
-
-    res.send(
-      `You can authorize access at ${HOST}/github/auth/${token} in the next 5 minutes.`,
+  withConfigOrReportIssues(
+    issueReporterForRobot(robot),
+    "GITHUB_CLIENT_ID",
+    "GITHUB_CLIENT_SECRET",
+  )((githubClientId, githubClientSecret) => {
+    robot.router.use(cookieParser())
+    passport.use(
+      new GitHubStrategy(
+        {
+          clientID: githubClientId,
+          clientSecret: githubClientSecret,
+          callbackURL: `${HOST}/github/auth`,
+          userAgent: "https://thesis.co",
+        },
+        function(accessToken, refreshToken, profile, done) {
+          done(null, { token: accessToken, profile: profile })
+        },
+      ),
     )
-  })
 
-  robot.router.get("/github/auth/:token", (req, res, next) => {
-    let token = req.params.token,
-      pendingGitHubTokens = robot.brain.get("pendingGitHubTokens") || {},
-      found = false
+    function cleanPending() {
+      let now = new Date().getTime(),
+        pendingGitHubTokens = robot.brain.get("pendingGitHubTokens") || {}
 
-    for (let [userId, pendingInfo] of Object.entries(pendingGitHubTokens)) {
-      if (token == pendingInfo.token) {
-        found = true
-        res.cookie("gh-auth-token", token, {
-          httpOnly: true,
-          //secure: true, TODO turn this on...
-          sameSite: "Strict",
-        })
-        break
+      for (let [userID, pendingInfo] of Object.entries(pendingGitHubTokens)) {
+        if (now - pendingInfo.date > 5 * MINUTE) {
+          delete robot.brain.pendingGitHubTokens[userID]
+        }
       }
+
+      robot.brain.set("pendingGitHubTokens", pendingGitHubTokens)
     }
 
-    if (found) {
-      passport.authorize("github", { scope: ["admin:org"] })(req, res, next)
-    } else {
-      res.send(404, "File Not Found.")
-    }
-  })
+    setInterval(cleanPending, 30 * SECOND)
+    cleanPending()
 
-  robot.router.get(
-    "/github/auth",
-    passport.authenticate("github", {
-      failureRedirect: "/github/auth/fail",
-      session: false,
-      assignProperty: "gitHubUser",
-    }),
-    (req, res) => {
-      let token = req.cookies["gh-auth-token"],
-        gitHubToken = req.gitHubUser.token,
+    robot.respond(/github auth/, res => {
+      let user = res.message.user,
+        token = UUIDV4()
+
+      let pendingGitHubTokens = robot.brain.get("pendingGitHubTokens") || {}
+      pendingGitHubTokens[user.id] = {
+        token: token,
+        date: new Date().getTime(),
+      }
+      robot.brain.set("pendingGitHubTokens", pendingGitHubTokens)
+
+      res.send(
+        `You can authorize access at ${HOST}/github/auth/${token} in the next 5 minutes.`,
+      )
+    })
+
+    robot.router.get("/github/auth/:token", (req, res, next) => {
+      let token = req.params.token,
         pendingGitHubTokens = robot.brain.get("pendingGitHubTokens") || {},
-        gitHubTokens = robot.brain.get("gitHubTokens") || {},
         found = false
 
       for (let [userId, pendingInfo] of Object.entries(pendingGitHubTokens)) {
         if (token == pendingInfo.token) {
-          delete pendingGitHubTokens[userId]
-          gitHubTokens[userId] = gitHubToken
-
-          robot.brain.set("pendingGitHubTokens", pendingGitHubTokens)
-          robot.brain.set("gitHubTokens", gitHubTokens)
-
           found = true
+          res.cookie("gh-auth-token", token, {
+            httpOnly: true,
+            //secure: true, TODO turn this on...
+            sameSite: "Strict",
+          })
           break
         }
       }
 
-      res.cookie("gh-auth-token", "")
       if (found) {
-        res.send(200, "<!doctype html><html><body>Got it!</body></html>")
+        passport.authorize("github", { scope: ["admin:org"] })(req, res, next)
       } else {
         res.send(404, "File Not Found.")
       }
-    },
-  )
+    })
+
+    robot.router.get(
+      "/github/auth",
+      passport.authenticate("github", {
+        failureRedirect: "/github/auth/fail",
+        session: false,
+        assignProperty: "gitHubUser",
+      }),
+      (req, res) => {
+        let token = req.cookies["gh-auth-token"],
+          gitHubToken = req.gitHubUser.token,
+          pendingGitHubTokens = robot.brain.get("pendingGitHubTokens") || {},
+          gitHubTokens = robot.brain.get("gitHubTokens") || {},
+          found = false
+
+        for (let [userId, pendingInfo] of Object.entries(pendingGitHubTokens)) {
+          if (token == pendingInfo.token) {
+            delete pendingGitHubTokens[userId]
+            gitHubTokens[userId] = gitHubToken
+
+            robot.brain.set("pendingGitHubTokens", pendingGitHubTokens)
+            robot.brain.set("gitHubTokens", gitHubTokens)
+
+            found = true
+            break
+          }
+        }
+
+        res.cookie("gh-auth-token", "")
+        if (found) {
+          res.send(200, "<!doctype html><html><body>Got it!</body></html>")
+        } else {
+          res.send(404, "File Not Found.")
+        }
+      },
+    )
+  })
 }
