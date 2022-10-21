4a6a0d3251d22a01145e00975ff6f5012cbe9219
Antonio Salazar Cardozo <antonio@thesis.co>
2022-10-21 02:29:26 -0400

    Make example test firmly TypeScript

--- test/examplep-test.ts
+++ test/examplep-test.ts
@@ -1,9 +1,10 @@
-import HubotChatTesting from "hubot-chat-testing"
-import Helper from "hubot-test-helper"
+import * as HubotChatTesting from "hubot-chat-testing"
+// @ts-expect-error No types!
+import * as Helper from "hubot-test-helper"
 
-const chat = new HubotChatTesting(
+const chat = new (HubotChatTesting as any)(
   "hubot",
-  new Helper("../scripts/example.coffee"),
+  new (Helper as any)("../scripts/example.coffee"),
 )
 
 describe("Testing example scripts", () => {
