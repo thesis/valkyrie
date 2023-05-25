92391eeb2fb169def462410e4e3fd8b5d3baa748
Erik Zuuring <erik@circlesphere.co>
2023-05-25 13:45:07 +0200

    Add tagging users from n8n email flow

--- discord-scripts/discord-webhook.ts
+++ discord-scripts/discord-webhook.ts
@@ -6,24 +6,26 @@ const discordClient = new Client({ intents: [] })
 discordClient.login(process.env.HUBOT_DISCORD_TOKEN)
 
 const discordWebhook = {
-  async sendToDiscordChannel(channelId: string, title: string, message: string) {
+  async sendToDiscordChannel(channelId: string, tagUser: string, title: string, message: string) {
     const channel = await discordClient.channels.fetch(channelId)
     if (channel && channel.isTextBased()) {
-      const channelAsText = channel as TextChannel;
-      const existingThread = channelAsText.threads.cache.find(thread => thread.name === title);
+      const channelAsText = channel as TextChannel
+      const existingThread = channelAsText.threads.cache.find(thread => thread.name === title)
 
         if (existingThread) {
-          await existingThread.send(message);
+          await existingThread.send(`@${tagUser}`)
+          await existingThread.send(message)
         } else {
           const newThread = await channelAsText.threads.create({
             name: title,
             autoArchiveDuration: 60,
             reason: message,
           });
-          await newThread.send(message);
+          await newThread.send(`@${tagUser}`)
+          await newThread.send(message)
         }
       } else {
-        throw new Error("Channel is not text-based or not found");
+        throw new Error("Channel is not text-based or not found")
     }
   }
 }
