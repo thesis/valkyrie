437eb35766e52daee4cba2ede5e0136251d34d0a
Erik Zuuring <erik@circlesphere.co>
2023-05-26 11:30:25 +0200

    linter fixes + adding users to threads

--- discord-scripts/discord-webhook.ts
+++ discord-scripts/discord-webhook.ts
@@ -1,33 +1,46 @@
 import { Client, TextChannel } from "discord.js"
-import { Robot } from "hubot"
 
 const discordClient = new Client({ intents: [] })
 
 discordClient.login(process.env.HUBOT_DISCORD_TOKEN)
 
 const discordWebhook = {
-  async sendToDiscordChannel(channelId: string, tagUser: string, title: string, message: string) {
+  async sendToDiscordChannel(
+    channelId: string,
+    tagUser: string,
+    title: string,
+    message: string,
+  ) {
     const channel = await discordClient.channels.fetch(channelId)
     if (channel && channel.isTextBased()) {
       const channelAsText = channel as TextChannel
-      const existingThread = channelAsText.threads.cache.find(thread => thread.name === title)
+      const memberIds = tagUser.split(",")
+      const existingThread = channelAsText.threads.cache.find(
+        (thread) => thread.name === title,
+      )
 
-        if (existingThread) {
-          await existingThread.send(`@${tagUser}`)
-          await existingThread.send(message)
-        } else {
-          const newThread = await channelAsText.threads.create({
-            name: title,
-            autoArchiveDuration: 60,
-            reason: message,
-          });
-          await newThread.send(`@${tagUser}`)
-          await newThread.send(message)
-        }
+      if (existingThread) {
+        await existingThread.send("@here")
+        await existingThread.send(message)
       } else {
-        throw new Error("Channel is not text-based or not found")
+        const newThread = await channelAsText.threads.create({
+          name: title,
+          autoArchiveDuration: 60,
+          reason: message,
+        })
+        await newThread.send("@here")
+        // replace with new user tagging once deployed in new DiscordJS version. Will be updated to add users to the thread dynamically based on
+        if (tagUser !== "0") {
+          await Promise.all(
+            memberIds.map((id) => newThread.members.add(id.trim())),
+          )
+        }
+        await newThread.send(message)
+      }
+    } else {
+      throw new Error("Channel is not text-based or not found")
     }
-  }
+  },
 }
 
 export default discordWebhook
