cdbaae7b5d6aca5d415e7bff2d3edca16b626426
Antonio Salazar Cardozo <antonio@thesis.co>
2023-08-18 23:55:39 -0400

    Add `isInTestingChannel` thread management util
    This checks a thread to see if it is inside a channel that we consider
    to be a testing channel—currently just the playground and bifrost
    channels.
    
    In certain cases, the testing channels will be used as “things don't
    happen here”, but when developing new features they might be used as
    “things only happen here”.

--- lib/discord/utils.ts
+++ lib/discord/utils.ts
@@ -3,9 +3,13 @@ import {
   ChannelType,
   AnyThreadChannel,
   ClientEvents,
+  ThreadChannel,
 } from "discord.js"
 import { Robot } from "hubot"
 import { DiscordBot } from "hubot-discord"
+
+// Channels that are used for testing, may be treated differently.
+const TESTING_CHANNEL_NAMES = ["playground", "bifrost"]
 
 /**
  * Hubot Robot type with Discord adapter.
@@ -24,6 +28,18 @@ export type DiscordEventHandlers = {
 
 // Category that is treated as recreational, i.e. the rules don't apply baby.
 export const RECREATIONAL_CATEGORY_ID = "1079492118692757605"
+
+/**
+ * Checks if a given thread is within a channel used for Hubot testing. At
+ * times, these channels may be subjected to laxer restrictions.
+ */
+export function isInTestingChannel(threadChannel: ThreadChannel): boolean {
+  return (
+    TESTING_CHANNEL_NAMES.indexOf(
+      threadChannel.parent?.name?.toLowerCase() ?? "",
+    ) !== -1
+  )
+}
 
 /**
  * Checks if a given channel is within the category considered "recreational".
