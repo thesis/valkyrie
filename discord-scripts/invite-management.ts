61069f5c646b836b6509bbdd430a7b4fa2443ab6
Erik Zuuring <erik@circlesphere.co>
2024-03-13 13:34:30 +0200

    Review changes 3/13
    This commits goes through the review changes and resolves these issues:
    
    - Remove extra consts on `maxAge/maxUses`
    - Rather than passing `channel.id`, pass the object instead
    - Suggested fix on split/slice for audit channel name (thank youuu!)

--- discord-scripts/invite-management.ts
+++ discord-scripts/invite-management.ts
@@ -1,28 +1,25 @@
 import { Robot } from "hubot"
 import { Client, TextChannel } from "discord.js"
-import { WEEK } from "../lib/globals.ts"
+import { DAY, MILLISECOND, WEEK } from "../lib/globals.ts"
 
 const EXTERNAL_AUDIT_CHANNEL_REGEXP = /^ext-(?<client>.*)-audit$/
 
 async function createInvite(
-  discordClient: Client,
-  channelId: string,
-  maxAge = WEEK,
+  channel: TextChannel,
+  maxAge = (1 * WEEK) / MILLISECOND,
   maxUses = 10,
-): Promise<string | null> {
-  const channelForInvite = await discordClient.channels.fetch(channelId)
-
-  if (channelForInvite === null || !("createInvite" in channelForInvite)) {
-    throw new Error("Channel not found or access denied.")
-  }
-
-  const invite = await channelForInvite.createInvite({
+): Promise<{ url: string; maxAge: number; maxUses: number }> {
+  const invite = await channel.createInvite({
     maxAge,
     maxUses,
     unique: true,
   })
 
-  return invite.url
+  return {
+    url: invite.url,
+    maxAge,
+    maxUses,
+  }
 }
 
 export default async function sendInvite(discordClient: Client, robot: Robot) {
@@ -48,30 +45,28 @@ export default async function sendInvite(discordClient: Client, robot: Robot) {
       if (
         !interaction.isCommand() ||
         interaction.commandName !== "create-invite"
-      )
+      ) {
         return
+      }
 
       if (!interaction.guild) {
         await interaction.reply("This command can only be used in a server.")
         return
       }
 
-      const maxAge = WEEK // default 7 days
-      const maxUses = 10 // default 10 uses
-
       try {
         const { channel } = interaction
         if (channel instanceof TextChannel) {
-          const invite = await channel.createInvite({
-            maxAge,
-            maxUses,
-            unique: true,
-          })
-          await interaction.reply(
-            `Here is your invite link: ${invite.url}\nThis invite expires in ${
-              maxAge / (3600 * 24)
-            } days and has a maximum of ${maxUses} uses.`,
-          )
+          const invite = await createInvite(channel)
+          if (invite) {
+            await interaction.reply(
+              `Here is your invite link: ${
+                invite.url
+              }\nThis invite expires in ${
+                (invite.maxAge / DAY) * MILLISECOND
+              } days and has a maximum of ${invite.maxUses} uses.`,
+            )
+          }
         } else {
           await interaction.reply(
             "Cannot create an invite for this type of channel.",
@@ -91,31 +86,29 @@ export default async function sendInvite(discordClient: Client, robot: Robot) {
         EXTERNAL_AUDIT_CHANNEL_REGEXP.test(channel.name)
       ) {
         try {
-          const channelId = channel.id
-          const maxAge = WEEK
-          const maxUses = 10
-          const inviteUrl = await createInvite(
-            discordClient,
-            channelId,
-            maxAge,
-            maxUses,
-          )
-          if (inviteUrl) {
+          const defenseInvite = await createInvite(channel)
+          if (defenseInvite) {
             robot.logger.info(
-              `New invite created for defense audit channel: ${channel.name}, URL: ${inviteUrl}`,
+              `New invite created for defense audit channel: ${channel.name}, URL: ${defenseInvite.url}`,
             )
             channel.send(
-              `Here is your invite link: ${inviteUrl}\n` +
-                `This invite expires in ${
-                  maxAge / (3600 * 24)
-                } days and has a maximum of ${maxUses} uses.`,
+              `Here is your invite link: ${
+                defenseInvite.url
+              }\nThis invite expires in ${
+                (defenseInvite.maxAge / DAY) * MILLISECOND
+              } days and has a maximum of ${defenseInvite.maxUses} uses.`,
             )
           }
           // Create a new role with the client name extracted and set permissions to that channel
-          const auditChannel = channel.name.split("-")
-          const clientName = auditChannel[0].includes("🔒")
-            ? auditChannel[1]
-            : ""
+          const clientName = channel.name
+            .split("-")
+            .slice(1, -1)
+            .map(
+              (segment) =>
+                segment.substring(0, 1).toUpperCase() + segment.substring(1),
+            )
+            .join(" ")
+
           const roleName = clientName
             ? `Defense: ${clientName}`
             : `Defense: ${channel.name}`
--- lib/globals.ts
+++ lib/globals.ts
@@ -1,6 +1,6 @@
 export const HOST = process.env.HUBOT_HOST
 export const MILLISECOND = 1000
-export const SECOND = 1
+export const SECOND = 1 * MILLISECOND
 export const MINUTE = 60 * SECOND
 export const HOUR = 60 * MINUTE
 export const DAY = 24 * HOUR
