539badd5320b0aeec29e31eb3dc04296dac6202a
Erik Zuuring <erik@circlesphere.co>
2024-10-31 15:25:12 +0200

    Add Acre channels for testing

--- lib/discord/utils.ts
+++ lib/discord/utils.ts
@@ -13,7 +13,7 @@ import { DiscordBot } from "hubot-discord"
 const BASE_ROLE_ID = "1158333090494689290"
 
 // Channels that are used for testing, may be treated differently.
-const TESTING_CHANNEL_NAMES = ["stackops"]
+const TESTING_CHANNEL_NAMES = ["stackops", "acre-engineering", "acre"]
 
 /**
  * Hubot Robot type with Discord adapter.
