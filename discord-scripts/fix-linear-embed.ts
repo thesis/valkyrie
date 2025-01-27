c694f20a9e0b01f766b4e3ce6559c77903fc26e3
Erik Zuuring <erik@circlesphere.co>
2025-01-27 17:09:22 +0200

    Remove issue from title

--- discord-scripts/fix-linear-embed.ts
+++ discord-scripts/fix-linear-embed.ts
@@ -127,7 +127,7 @@ async function createLinearEmbed(
 
       if (comment) {
         embed
-          .setTitle(`Comment on Issue: [${issue.identifier}]${issue.title}`)
+          .setTitle(`Comment on [${issue.identifier}]${issue.title}`)
           .setURL(
             `https://linear.app/${teamName}/issue/${issue.identifier}#comment-${commentId}`,
           )
@@ -157,7 +157,7 @@ async function createLinearEmbed(
           )
       } else {
         embed
-          .setTitle(`Issue: [${issue.identifier}] ${issue.title}`)
+          .setTitle(`[${issue.identifier}] ${issue.title}`)
           .setURL(`https://linear.app/${teamName}/issue/${issue.identifier}`)
           .setDescription(
             truncateToWords(issue.description, "No description available.", 50),
